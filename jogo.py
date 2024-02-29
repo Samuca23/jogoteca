501134cd978c4313421660afb2ee766b42691e53
Samuel <samuel.chiodini@unidavi.edu.br>
2024-02-29 09:16:12 -0300

    modulo 2 terminado

---
+++ jogo.py
@@ -0,0 +1,5 @@
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self._nome = nome
+        self._categoria = categoria
+        self._console = console
--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from jogo import Jogo
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/jogoteca')
 def hello():
-    return 'Hello, World!'
+    jogo_1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo_2 = Jogo('God od War', 'Rack n Slash', 'PS2')
+
+    jogo = [jogo_1, jogo_2]
+    return render_template('lista.html', titulo='Jogos', jogos=jogo)
 
 if __name__ == '__main__':
     app.run(debug=True)
