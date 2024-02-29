bf20ff5da53dc6d3477c45962cef15e7cd4a2f8f
Samuel <samuel.chiodini@unidavi.edu.br>
2024-02-29 09:45:57 -0300

    modulo 3 terminado

--- jogoteca.py
+++ jogoteca.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from jogo import Jogo
 
 app = Flask(__name__)
 
-@app.route('/jogoteca')
-def hello():
-    jogo_1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo_2 = Jogo('God od War', 'Rack n Slash', 'PS2')
+lista = []
+jogo_1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo_2 = Jogo('God od War', 'Rack n Slash', 'PS2')
+lista = [jogo_1, jogo_2]
 
-    jogo = [jogo_1, jogo_2]
-    return render_template('lista.html', titulo='Jogos', jogos=jogo)
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo() :
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST',]) 
+def criar() : 
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 if __name__ == '__main__':
     app.run(debug=True)
