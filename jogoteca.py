77b719686d7a4771fbfb8a2127bc60c7252d1015
Samuel <samuel.chiodini@unidavi.edu.br>
2024-03-04 09:22:10 -0300

    modulo 6 terminado

--- jogoteca.py
+++ jogoteca.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from jogo import Jogo
+from usuario import Usuario
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 lista = []
 jogo_1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo_2 = Jogo('God od War', 'Rack n Slash', 'PS2')
 lista = [jogo_1, jogo_2]
+
+usuario1 = Usuario('Samuel', 'Samuca', 'abc')
+usuario2 = Usuario('Isadora', 'Isa', 'isa123')
+usuarios = { usuario1.nickname : usuario1, usuario2.nickname : usuario2 }
 
 @app.route('/')
 def index():
@@ -14,6 +20,9 @@ def index():
 
 @app.route('/novo')
 def novo() :
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo'))) 
+    
     return render_template('novo.html', titulo='Novo Jogo')
 
 
@@ -24,7 +33,33 @@ def criar() :
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login() :
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar() :
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha :
+            session['usuario_logado'] = usuario.nickname
+            flash('Login efetuado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+    else :
+        flash('Login incorreto!')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout() :
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
