c9eb7ce688cc09ce4acbe24e4299e2041e403b04
Samuel <samuel.chiodini@unidavi.edu.br>
2024-03-11 10:44:40 -0300

    modulo 2 finalizado

--- controller/jogos.py
+++ controller/jogos.py
@@ -15,7 +15,7 @@ def novo() :
     return render_template('novo.html', titulo='Novo Jogo')
 
 
-@app.route('/criar', methods=['POST',]) 
+@app.route('/criar', methods=['POST']) 
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -30,4 +30,35 @@ def criar():
         db.session.add(novo_jogo)
         db.session.commit()
 
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>')
+def editar(id) :
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar'))) 
+    
+    jogo = Jogos.query.filter_by(id=id).first()
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+
+@app.route('/atualizar', methods=['POST']) 
+def atualizar():
+    jogo = Jogos.query.filter_by(id=request.form['id']).first()
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+
+    db.session.add(jogo)
+    db.session.commit()
+
+    return redirect( url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id) :
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login'))
+    
+    Jogos.query.filter_by(id=id).delete()
+    db.session.commit()
+    flash('Jogo deletado com sucesso!')
+    
     return redirect(url_for('index'))
