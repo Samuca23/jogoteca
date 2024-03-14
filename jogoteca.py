d56349275aedff8e137b73cd5c475905b1c3b8fe
Samuel <samuel.chiodini@unidavi.edu.br>
2024-03-14 16:27:39 -0300

    modulo 6 finalizado

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from db import db
 from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -8,6 +9,7 @@ app.config.from_pyfile('config.py')
 # Inicialize o objeto db
 db.init_app(app)
 csrf = CSRFProtect(app)
+bcrypt = Bcrypt(app)
 
 from controller.jogos import *
 from controller.login import *
